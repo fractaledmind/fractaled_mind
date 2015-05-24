f3b939dd9be0660e33d7a47b0e8f647065a86049
Stephen Margheim <margheim@sas.upenn.edu>
2015-05-24 01:11:46 -0400

    final day build
    added search, new SVG logo, 12 columns on all viewports, general clean up

---
+++ javascripts/search.js
@@ -0,0 +1,38 @@
+(function() {
+  var $ = jQuery;
+
+  $(function() {
+    // URL ends: /search?q=mind
+    var search_term = window.location.search.substr(1).split('=')[1];
+    return $.getJSON('/search.json', function(data) {
+      var i, j, k, len, len1, result, results, results1, value;
+      results = [];
+      for (j = 0, len = data.length; j < len; j++) {
+        i = data[j];
+        value = 0;
+        if (i.title.toLowerCase().split(search_term).length - 1 !== 0) {
+          value = 10;
+        }
+        if (i.content.toLowerCase().split(search_term).length - 1 !== 0) {
+          value += (i.content.toLowerCase().split(search_term).length - 1) * 5;
+        }
+        if (value !== 0) {
+          i.value = value;
+          results.push(i);
+        }
+      }
+      $('#search_results').html('');
+      $('#results-title').append("<h2>Search Results for: '" + search_term + "'</h2>");
+      if (results.length > 0) {
+        results1 = [];
+        for (k = 0, len1 = results.length; k < len1; k++) {
+          result = results[k];
+          results1.push($('#search_results').append('<li><a class="search-result" href="/' + result.url + '">' + result.title + '</a> (' + result.type +')</li>'));
+        }
+        return results1;
+      } else {
+        $('#search_results').append('<p>No results found. Sorry.</p>');
+      }
+    });
+  });
+}).call(this);
