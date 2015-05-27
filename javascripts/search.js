00d115104d31601cc1aa2439c301d889e2de9bcc
Stephen Margheim <margheim@sas.upenn.edu>
2015-05-27 00:18:29 -0400

    final day build
    remove 3 layout css files, clean up css, etc

--- javascripts/search.js
+++ javascripts/search.js
@@ -7,16 +7,17 @@
     if(search_substr) {
       var search_term = search_substr.split('=')[1].toLowerCase();
       return $.getJSON('/search.json', function(data) {
-        var i, j, k, len, len1, result, results, results1, value;
+        var i, j, k, len, len1, result, results, results1, value, content;
         results = [];
         for (j = 0, len = data.length; j < len; j++) {
           i = data[j];
           value = 0;
-          if (i.title.toLowerCase().split(search_term).length - 1 !== 0) {
+          content = $(i.content).text().toLowerCase();
+          if (i.title.toLowerCase().indexOf(search_term) !== -1) {
             value = 10;
           }
-          if (i.content.toLowerCase().split(search_term).length - 1 !== 0) {
-            value += (i.content.toLowerCase().split(search_term).length - 1) * 5;
+          if (content.indexOf(search_term) !== -1) {
+            value += (content.split(search_term).length - 1) * 5;
           }
           if (value !== 0) {
             i.value = value;
@@ -24,7 +25,7 @@
           }
         }
         $('#search_results').html('');
-        $('#results-title').append("<h2>Search Results for: '" + search_term + "'</h2>");
+        $('h2#title').append("Search Results for: '" + search_term + "'");
         if (results.length > 0) {
           results1 = [];
           for (k = 0, len1 = results.length; k < len1; k++) {
